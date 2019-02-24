df1027036471660707ab310c391f6bb5078e31a2
Henrique <ricogx@hotmail.com>
2019-02-24 14:12:10 -0300

    prod

--- src/main/java/br/com/henrique/config/WebSocketConfiguration.java
+++ src/main/java/br/com/henrique/config/WebSocketConfiguration.java
@@ -12,7 +12,7 @@ public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfig
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("https://app-ui-restaurante.herokuapp.com")
                 .withSockJS();
     }
 
